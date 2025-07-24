a697d0a0515f3e80064a0617d476830b0b83f351
qyaniwu <qyaniwu@tencent.com>
2025-07-24 10:05:24 +0800

    fix: 1.修复了文章无法显示图片

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
+const env=loadEnv('',process.cwd(),'');
+const {QYANI_COMPONENTS_PATH}=env
+console.log(`QYANI_COMPONENTS_PATH is ${QYANI_COMPONENTS_PATH}`)
 export default defineConfig({
   plugins: [vue()],
   resolve: {
