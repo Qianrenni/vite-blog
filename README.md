93cdee5fa183c66b4eac73a5e1c66581095818a0
qyaniwu <qyaniwu@tencent.com>
2025-08-22 16:32:41 +0800

    feat: 修改homePage的样式

--- README.md
+++ README.md
@@ -14,17 +14,3 @@ pnpm run dev
 ```
 pnpm run build
 ```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-
-# 远程更新
-
-```shell
-scp -r dist/* root@1.95.141.194:/usr/share/nginx/html/
-```
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-column container-align-center">
+  <div class=" container-column container-align-center">
     <!-- Hero Section -->
     <section class="text-secondary container-column container-align-center gap container-space-evenly" style="min-height: 400px;">
       <h1>你好！我是QYANI</h1>
@@ -8,11 +8,28 @@
     </section>
 
     <!-- 中间区域 - 展示有趣的页面效果 -->
-    <section class="text-secondary border-primary container-column container-space-around" style="max-width: 800px;width: 100%;height: 300px;">
-      <div class="animated-box bg-primary "/>
-      <div class="container-space-between">
-        <div class="animated-box bg-primary "/>
-        <div class="animated-box bg-primary "  />
+    <section class="show-container text-secondary  container-column container-space-around" style="max-width: 800px;width: 100%;height: 300px;">
+      <div class="container-space-around container-align-center">
+        <div class="loader-nature">
+          <div class="ripple"></div>
+          <div class="ripple"></div>
+          <div class="ripple"></div>
+        </div>
+        <div class="loader-tech"></div>
+        <div class="loader-nature">
+          <div class="ripple"></div>
+          <div class="ripple"></div>
+          <div class="ripple"></div>
+        </div>
+      </div>
+      <div class="container-space-around container-align-center">
+        <div class="loader-tech"></div>
+        <div class="loader-nature">
+          <div class="ripple"></div>
+          <div class="ripple"></div>
+          <div class="ripple"></div>
+        </div>
+        <div class="loader-tech"></div>
       </div>
     </section>
 
@@ -32,34 +49,11 @@ defineOptions({
 });
 </script>
 <style scoped>
-
-.animated-box {
-  width: 100px;
-  height: 100px;
-  animation: bounce-spin 2s infinite ease-in-out;
-  margin: 0 auto;
-  border-radius: 8px;
-}
-@keyframes bounce-spin {
-  0%, 100% {
-    opacity: 1;
-    transform: translateY(0) rotateZ(0deg);
-  }
-  50% {
-    transform: translateY(-25px) rotateZ(180deg);
-    opacity: 0.5;
-  }
-  75% {
-    transform: translateY(-10px) rotateZ(540deg);
-  }
-}
-
-
-/* 响应式媒体查询 */
-@media (max-width: 768px) {
-  .animated-box {
-    width: 80px;
-    height: 80px;
-  }
+.show-container{
+  background: url('public/images/bg1.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 50%;
 }
 </style>
