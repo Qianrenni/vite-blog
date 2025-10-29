b677eece389f6e30e33a967161c4e7e8b7b75d93
Qianrenni <3092176591@qq.com>
2025-10-29 21:39:58 +0800

    feat: 1.删除HomePage.vue

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'qyani-components/dist/style.css'
 import '../public/assets/css/private.css'
-import qcomponents from 'qyani-components'
 import router from './router'
 import {createPinia} from 'pinia'
 createApp(App)
     .use(router)
     .use(createPinia())
-    .use(qcomponents)
     .mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 
 
 const routes = [
-    { path: '/', component: ()=>import('../views/HomePage.vue') },
+    { path: '/', component: ()=>import('../views/CourseNav.vue') },
     { path:'/chat-ai',component:()=>import('../views/ChatAI.vue')},
     {
         path: '/courses',component: ()=>import('../views/CourseNav.vue'),
